8323f348a9b4097aef822a12fedd0ce535b1394a
robdan89 <fuquardwork@gmail.com>
2020-03-17 10:58:24 -0500

    created notes.json inside of api directory. Now new notes should save save to notes.json when save button is clicked.

--- scripts/notes/NoteForm.js
+++ scripts/notes/NoteForm.js
@@ -1,15 +1,40 @@
-<input type="text" id="note-text"></input>
+import { saveNote } from "./NoteProvider.js";
 
-const contentTarget = document.querySelector(".noteFormContainer")
+const contentTarget = document.querySelector(".noteFormContainer");
+const eventHub = document.querySelector(".container");
 
 const render = () => {
-    contentTarget.innerHTML = `
-        Put some input fields and prompts here
-
+  contentTarget.innerHTML = `
+        <fieldset>
+        <label for="noteText">Note:</label>
+        <input type="text" id="noteText">
+        </fieldset>
+        
+        <fieldset>
+        <label for="criminalName">Criminal:</label>
+        <input type="text" id="criminalName">
+        </fieldset>
+        
         <button id="saveNote">Save Note</button>
-    `
-}
+    `;
+};
 
-export const NoteForm = () => {
-    render()
-}
+const NoteForm = () => {
+  render();
+};
+
+contentTarget.addEventListener("click", clickEvent => {
+  // Make sure it was one of the color buttons
+  if (clickEvent.target.id === "saveNote") {
+    const noteText = document.querySelector("#noteText").value;
+    const criminalName = document.querySelector("#criminalName").value;
+    const newNote = {
+      noteText: noteText,
+      criminalName: criminalName,
+      timestamp: Date.now()
+    };
+    saveNote(newNote);
+  }
+});
+
+export default NoteForm;
--- scripts/notes/NoteProvider.js
+++ scripts/notes/NoteProvider.js
@@ -1,28 +1,29 @@
-const eventHub = document.querySelector(".container")
+const eventHub = document.querySelector(".container");
+let notes = [];
+export const useNotes = () => notes.slice();
 
 const dispatchStateChangeEvent = () => {
-    const noteStateChangedEvent = new CustomEvent("noteStateChanged")
+  const noteStateChangedEvent = new CustomEvent("noteStateChanged");
 
-    eventHub.dispatchEvent(noteStateChangedEvent)
-}
+  eventHub.dispatchEvent(noteStateChangedEvent);
+};
 
 const getNotes = () => {
-    fetch('http://localhost:8080/notes')
-        .then(response => response.json())
-        .then(parsedNotes => {
-            notes = parsedNotes
-        })
-
-}
+  return fetch("http://localhost:8088/notes")
+    .then(response => response.json())
+    .then(parsedNotes => {
+      notes = parsedNotes;
+    });
+};
 
 export const saveNote = note => {
-    fetch('http://localhost:8080/notes', {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify(note)
-    })
+  return fetch("http://localhost:8088/notes", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(note)
+  })
     .then(getNotes)
-    .then(dispatchStateChangeEvent)
-}
+    .then(dispatchStateChangeEvent);
+};

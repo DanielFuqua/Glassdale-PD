829ba379e62b479b26e3d03fab12217fb065fe8e
robdan89 <fuquardwork@gmail.com>
2020-03-19 11:45:10 -0500

    added notes.json. Created ther display all notes button which displays notesList. Added display note form button which toggles noteForm on and off. Added save note functionality to the saveNote button, which save the inpout data to the notes.json.

--- scripts/criminals/Criminal.js
+++ scripts/criminals/Criminal.js
@@ -1,13 +1,19 @@
-export const Criminal = (criminalObject) => {
-    return `
+export const Criminal = criminalObject => {
+  return `
     <div class="criminal">
         <h4>${criminalObject.name}</h4>
         <ul class="ul">
             <li>Age: ${criminalObject.age}</li>
             <li>Conviction: ${criminalObject.conviction}</li>
-            <li>Term Start: ${new Date(criminalObject.incarceration.start).toLocaleDateString('en-US')}</li>
-            <li>Term End: ${new Date(criminalObject.incarceration.end).toLocaleDateString('en-US')}</li>
+            <li>Term Start: ${new Date(
+              criminalObject.incarceration.start
+            ).toLocaleDateString("en-US")}</li>
+            <li>Term End: ${new Date(
+              criminalObject.incarceration.end
+            ).toLocaleDateString("en-US")}</li>
         </ul>
+        <button id="associates--${criminalObject.id}">Associate Alibis</button>
     </div>
-    `
-}
+
+    `;
+};
